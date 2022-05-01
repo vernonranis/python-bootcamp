39cf2042427291b4759496f9eacdafa2985eafb6
Vernon Ranis <vernonranis@gmail.com>
2022-05-01 02:02:18 -0400

    done

---
+++ app.py
@@ -0,0 +1,19 @@
+import random
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+ # 🚨 Don't change the code above 👆 It's only for testing your code.
+	 
+#Write the rest of your code below this line 👇
+
+random_number = random.randint(0, 1)
+
+if random_number == 1:
+  print("Heads")
+else:
+  print("Tails")
+
+# states_of_america = [""]
