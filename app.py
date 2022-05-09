d4ca3b502a2f3c08d926d723ec8bbcd3aec12f80
Vernon Ranis <vernonranis@gmail.com>
2022-05-09 06:14:57 -0400

    done

---
+++ app.py
@@ -0,0 +1,9 @@
+# Review: 
+# Create a function called greet(). 
+def greet(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}")
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+greet(name = "Vernon", location = "Earth")
