1b1eebb9eaa59b8d72b40bcf5017604adeb2b252
Vernon Ranis <vernonranis@gmail.com>
2022-05-08 22:28:58 -0400

    Done Todo 3

--- app.py
+++ app.py
@@ -8,30 +8,39 @@ chosen_word = choice(word_list)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
+#Create blanks
 guess = ""
 display = []
-while len(guess) == 0:
-  guess = input("Guess a letter: ").lower()
-  if len(guess) > 1 or len(guess) < 1:
-    system("clear")
-    guess = ""
-  else:
-    pass
+end_of_game = False
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+#TODO-1: - Use a while loop to let the user guess again. 
+# The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_").
+# Then you can tell the user they've won.
 
-for index in chosen_word:
-  if guess == index:
-    display.append(guess)
-  else:
+print (len(display))
+
+for word in chosen_word:
     display.append("_")
 
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
+while not end_of_game:
+    while len(guess) == 0:
+        guess = input("Guess a letter: ").lower()
+        if len(guess) > 1 or len(guess) < 1:
+            system("clear")
+            guess = ""
+        else:
+            pass
+    for index in range(len(chosen_word)):
+        if guess in chosen_word[index]:
+            display[index] = guess
+            system("clear")
+        else:
+            system("clear")
+            pass
+
+    if "_" not in display:
+        end_of_game = True
+    print(display)
+    guess = ""
+
+print("You win")
