b4d3c343b7526b308756c20f91beed534d901af0
Vernon Ranis <vernonranis@gmail.com>
2022-05-15 10:04:15 -0400

    done caecar 4

--- app.py
+++ app.py
@@ -1,26 +1,66 @@
+from os import execl, system
+from sys import executable, argv, exit
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+# print(45 % 45)
+
+system("clear")
+print(logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-
 def caecar(message, dir, shift_amount):
     msg = list(message)
     final_msg = []
+    if shift_amount > len(alphabet):
+        shift_amount %= len(alphabet)
+
     if dir == "decode":
         shift_amount *= -1
+
     for index in range(len(text)):
-        orig_char_loc = alphabet.index(msg[index])
-        new_char_loc = orig_char_loc + shift_amount
-        if new_char_loc > 25:
-            ext_char_loc = new_char_loc - len(alphabet)
-            final_msg.append(alphabet[ext_char_loc])
-        else:
-            final_msg.append(alphabet[new_char_loc])
+    #TODO-3: What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+        try:
+            orig_char_loc = alphabet.index(msg[index])
+            new_char_loc = orig_char_loc + shift_amount
+            # if new_char_loc > 25:
+            if new_char_loc > len(alphabet):
+                ext_char_loc = new_char_loc - len(alphabet)
+                final_msg.append(alphabet[ext_char_loc])
+            else:
+                final_msg.append(alphabet[new_char_loc])
+        except ValueError:
+            final_msg.append(msg[index])
+            continue
+            
     print("".join(final_msg))
 
-#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+#Hint: Think about how you can use the modulus (%).
 
 caecar(message = text, dir = direction, shift_amount = shift)
+
+again = input("do you want to try again?(y/n)")
+
+while again:
+    if again == "y":
+        system("clear")
+        execl(executable, executable, *argv)
+    elif again =="n":
+        exit(0)
+    else:
+        system("clear")
+        print("Invalid Option! Enter Again")
