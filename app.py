bdf8ae6ba82f85ccead638e7f2ba4e9505d29543
Vernon Ranis <vernonranis@gmail.com>
2022-05-16 04:23:09 -0400

    done

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
- ##Python Dictionaries
+##Python Dictionaries
 
 programming_dictionary = {
-  "Bug": "An error in a program that prevents the program from running as expected.", 
-  "Function": "A piece of code that you can easily call over and over again.",
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again.",
 }
 
 #Retrieving items from dictionary.
@@ -31,37 +31,37 @@ programming_dictionary["Bug"] = "A moth in your computer."
 
 #Nesting 
 capitals = {
-  "France": "Paris",
-  "Germany": "Berlin",
+    "France": "Paris",
+    "Germany": "Berlin",
 }
 
 #Nesting a List in a Dictionary
 
 travel_log = {
-  "France": ["Paris", "Lille", "Dijon"],
-  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
 }
 
 #Nesting Dictionary in a Dictionary
 
 travel_log = {
-  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
 }
 
 #Nesting Dictionary in a List
 
 travel_log = [
-  {
+    {
     "country": "France", 
     "cities_visited": ["Paris", "Lille", "Dijon"], 
     "total_visits": 12
-  },
-  {
+    },
+    {
     "country": "Germany", 
     "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
     "total_visits": 5
-  },
+    },
 ]
 
 
