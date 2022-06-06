ddb750930cc3209d4b097a95ed870412830da233
vernon ranis <vernonranis@gmail.com>
2022-06-06 01:40:57 -0400

    linted

--- app.py
+++ app.py
@@ -44,7 +44,9 @@ def calculator():
     calculation_function = operation[operation_symbol]
     first_answer = calculation_function(num1, num2)
     print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-    keep_looping = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to start a new calculation: ")
+    keep_looping = input(
+        f"Type 'y' to continue calculating with {first_answer}, or type 'n' to start a new calculation: "
+    )
     if keep_looping == "y":
         continue_operation = True
         system("clear")
@@ -61,13 +63,19 @@ def calculator():
         num3 = float(input("What is the next number?: "))
         if second_answer == None:
             second_answer = calculation_function(first_answer, num3)
-            print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+            print(
+                f"{first_answer} {operation_symbol} {num3} = {second_answer}"
+            )
         elif second_answer != None:
             previous_answer = second_answer
             second_answer = calculation_function(second_answer, num3)
-            print(f"{previous_answer} {operation_symbol} {num3} = {second_answer}")
+            print(
+                f"{previous_answer} {operation_symbol} {num3} = {second_answer}"
+            )
         calculation_function = operation[operation_symbol]
-        keep_looping = input(f"Type 'y' to continue calculating with {second_answer}, or type 'n' to start a new calculation: ")
+        keep_looping = input(
+            f"Type 'y' to continue calculating with {second_answer}, or type 'n' to start a new calculation: "
+        )
         if keep_looping == "y":
             continue_operation = True
         elif keep_looping == "n":
